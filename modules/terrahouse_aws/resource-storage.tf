86f31450e1ca3f99712788200ea4e446909a8035
Brouser <r.ivasyuk@gmail.com>
2023-10-13 20:39:41 +0300

    #31  invlidate cloud front stack when content version changes (#32)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -47,6 +47,7 @@ resource "aws_s3_object" "error_html" {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes = [etag]
 }
+}
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
