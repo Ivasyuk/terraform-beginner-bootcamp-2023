21eda16283a305af324b47b5e1f7c8f7742de2ef
Brouser <r.ivasyuk@gmail.com>
2023-10-13 12:42:37 +0300

    #23 refactor s3 bucker to terrahouse module (#24)

--- main.tf
+++ main.tf
@@ -1,18 +1,7 @@
 
-resource "random_string" "bucket_name" {
-  lower    = true
-  upper    = false
-  length   = 32
-  special  = false
- 
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
-
-resource "aws_s3_bucket" "website_bucket" {
- 
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
-}
-
